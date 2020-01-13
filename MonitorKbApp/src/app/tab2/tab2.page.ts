f18dedcd95b4e31b6ba5624a34fcbf98c2c18364
KauaAquino <kauaplayer22@gmail.com>
2020-01-13 14:16:57 -0300

    ajustes tela de cobrança

--- MonitorKbApp/src/app/tab2/tab2.page.ts
+++ MonitorKbApp/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
-
+  constructor(public alertController : AlertController) {}
+  async alerta(){
+  const alert = await this.alertController.create({
+    header: ' O Que Deseja ? ',
+    buttons:[],
+  })
+  await alert.present();
 }
+}
+
